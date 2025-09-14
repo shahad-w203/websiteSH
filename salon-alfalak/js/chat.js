a670336393cf3ec88962819a68c24f173d3e8971
shahad-w203 <shahadmh123123@gmail.com>
2025-09-14 15:19:07 +0300

    Add files via upload

---
+++ salon-alfalak/js/chat.js
@@ -0,0 +1,3 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "80788d7e-e8aa-4e6f-9c1d-c265f286f8fb";
+(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
---
+++ salon-alfalak/js/script.js
@@ -0,0 +1,7 @@
+let icon = document.getElementById('icon-mobile');
+let ul = document.getElementById('ul-mobile');
+
+
+icon.onclick = function(){
+    ul.classList.toggle("mobile-nav1")
+}
